0e1acf2441f869d13d3b730e17d52cc888061d0d
GraceMwende <grace.micheni@student.moringaschool.com>
2021-11-14 15:25:25 +0300

    get akan names

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-// function getDate(){
-//   var date = document.querySelector('#date').value;
-//   return date;
-// }
-// alert(getDate(date));
+ const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+ const maleNames = ["kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "kwame"];
+ const femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-// const wrapper = document.querySelector('.form'),
-//       form =document.querySelectorAll('form'),
-//       submitInput = form[0].querySelector('input[type = "submit"]');
-
-
-// function getdataForm(e){
-//   e.preventDefault();
-
-//   var formData = new FormData(form[0]);
-//   alert(formData.get('gender') + ' - ' + FormData.get('date'));
-// }
-
-// document.addEventListener('DOMContentLoaded', function(){
-//   submitInput.addEventListener('click', getdataForm, false)
-// }, false);  
-
+ document.getElementById("formValues").addEventListener("submit", function(e){
+   e.preventDefault();
+   takevalue();
+ })
  function takevalue(){
     var gender = document.getElementById("gender").value;
-    var date = document.getElementById("date").value;
-    document.write("Your gender is:" + gender);
-    document.write("Your birthday date is:" +date);
-    console.log(gender);
-    console.log(date);
+    var dateValue = document.getElementById("date").value;
+    // document.write("Your gender is:" + gender);
+    // document.write("Your birthday date is:" +dateValue);
+
+    let dayIndex = getDayIndex(dateValue);
+    getAkpanName(dayIndex, gender);
  }
+
+ function getDayIndex(dateValue){
+  var dateValueConverted = new Date (dateValue);
+  let day = dateValueConverted.getDay();
+  return day;
+ }
+
+ function getAkpanName(dayIndex, gender){
+  if (gender == "male"){
+      console.log(`You were born on ${days[dayIndex]}Akpan Name is ${maleNames[dayIndex]}`);
+  }
+  else if (gender == "female"){
+     console.log(`You were born on ${days[dayIndex]}Akpan Name is ${femaleNames[dayIndex]}`)
+  }
+ }
+
+
+
+
